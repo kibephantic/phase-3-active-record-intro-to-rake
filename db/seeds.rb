afe87074f443023c3744c131240b47d8fa015d35
PatrickKibe <phantickibe01@gmail.com>
2022-08-31 07:26:11 +0300

    commit tree 206a357cf53658777ccabef710077ac61b0f4aae

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,20 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+
+namespace :db do
+    desc 'migrate changes to your database'
+    task migrate: :environment do
+      Student.create_table
+    end
+
+    namespace :db do
+
+        # ...
+      
+        desc 'seed the database with some dummy data'
+        task seed: :environment do
+          require_relative './db/seeds'
+        end
+    end
